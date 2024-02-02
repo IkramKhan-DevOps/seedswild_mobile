9067471e1c425e7f53f9f5ddc0f11b5a06e5e861
IkramKhan-DevOps <ikram.khan0762@gmail.com>
2024-02-02 21:42:01 +0500

    SERVICE: plants initiated

---
+++ lib/services/plants/plants_list_screen.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class DummyScreen extends StatelessWidget {
+  const DummyScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "Some dummy text"
+        ),
+      ),
+    );
+  }
+}
